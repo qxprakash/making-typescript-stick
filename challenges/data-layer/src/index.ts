a5eff0b6e37316eddc781f87f1d6fc6205a3a14a
Prakash <qxprakash@gmail.com>
2025-04-26 18:21:49 +0000

    Datastore challenge half way

--- challenges/data-layer/src/index.ts
+++ challenges/data-layer/src/index.ts
@@ -13,4 +13,50 @@ export type DataEntityMap = {
   song: Song;
 };
 
-export class DataStore {}
+
+type DataStoreMethods = {
+  [K in keyof DataEntityMap as `GetAll${Capitalize<K>}s`]: () => DataEntityMap[K][]
+} & {
+  [K in keyof DataEntityMap as `Get${Capitalize<K>}ById`]: (id: string) => DataEntityMap[K]
+} & {
+  [K in keyof DataEntityMap as `clear${Capitalize<K>}s`]: () => void
+} & {
+  [K in keyof DataEntityMap as `add${Capitalize<K>}`]: (entity: DataEntityMap[K]) => DataEntityMap[K]
+}
+
+
+
+export class DataStore implements DataStoreMethods {
+  #data: { [K in keyof DataEntityMap]: Record<string, DataEntityMap[K]> } = {
+    movie: {},
+    song: {}
+  }
+
+  getAllSongs(): Song[] {
+    return Object.values(this.#data.song).map((songKey) => this.#data.song[songKey]);
+  }
+  getSong(id: string): Song {}
+  clearSongs(): void {}
+  addSong(entity: Song): Song {}
+
+  getAllMovies(): Movie[] {
+    return Object.values(this.#data.movie);
+  }
+  getMovie(id: string): Movie {
+    return this.#data.movie[id];
+  }
+  clearMovies(): void {}
+  addMovie(entity: Movie): Movie {
+    this.#data.movie[entity.id] = entity;
+    return entity;
+  }
+
+
+
+
+}
+
+
+const ds: DataStoreMethods = { } as any;
+
+
